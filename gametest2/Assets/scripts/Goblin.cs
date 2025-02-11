3e4480f852505a8c6287bd70c08254556a01cf4a
adrian <ae494152@gmail.com>
2025-02-11 14:10:55 -0500

    ai CONTINUED MULTIPLE BUGS

--- gametest2/Assets/scripts/Goblin.cs
+++ gametest2/Assets/scripts/Goblin.cs
@@ -4,9 +4,80 @@ public class Goblin : MonoBehaviour
 {
     [SerializeField] private float leftcap;
     [SerializeField] private float rightcap;
-    [SerializeField] private float jumpLength;
-    [SerializeField] private float jumpHeight;
+    [SerializeField] private float jumpLength = 2;
+    [SerializeField] private float jumpHeight = 2;
     [SerializeField] private LayerMask Ground;
 
+    private Collider2D coll;
+    private Rigidbody2D rb;
+
     private bool facingLeft = true;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (facingLeft)
+        {
+            if (transform.position.x > leftcap)
+            {
+                if (transform.localScale.x != 1)
+                {
+
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                if (coll.IsTouchingLayers(Ground))
+                {
+                    rb.linearVelocity = new Vector2(-jumpLength, jumpHeight);
+                }
+
+
+
+
+
+            }
+            else
+            {
+          
+                facingLeft = false;
+
+            }
+        
+
+
+
+
+
+        }
+        else
+        {
+            if (transform.position.x < rightcap)
+            {
+                if (transform.localScale.x != -1)
+                {
+
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                if (coll.IsTouchingLayers(Ground))
+                {
+                    rb.linearVelocity = new Vector2(jumpLength, jumpHeight);
+                }
+
+
+
+
+
+            }
+            else
+            {
+
+                facingLeft = true;
+
+            }
+        }
+    }
 }
